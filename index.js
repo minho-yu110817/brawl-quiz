d155cd975dd813c3b1215759f40e3b4eff2a2a5c
minho-yu110817 <meyh110817@naver.com>
2024-10-20 15:47:46 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,98 @@
+{
+  "comment-font-size": 0,
+  "white-stone-image": "white1.png",
+  "black-stone-image": "black1.png",
+  "shadow-size": 55,
+  "solid-stone-indicator": true,
+  "blunder-winrate-thresholds": [
+    -30,
+    -20,
+    -10,
+    -5,
+    5,
+    10
+  ],
+  "winrate-font-name": "Î¢ÈíÑÅºÚ",
+  "ui-font-name": "Î¢ÈíÑÅºÚ",
+  "minimum-blunder-bar-width": 3,
+  "blunder-bar-color": [
+    255,
+    0,
+    0,
+    200
+  ],
+  "winrate-miss-line-color": [
+    0,
+    0,
+    255,
+    200
+  ],
+  "comment-background-color": [
+    0,
+    0,
+    0,
+    200
+  ],
+  "winrate-stroke-width": 2,
+  "comment-node-color": [
+    0,
+    0,
+    255,
+    200
+  ],
+  "board-image": "board7.jpg",
+  "scoremean-line-color": [
+    255,
+    0,
+    255
+  ],
+  "font-name": "Î¢ÈíÑÅºÚ",
+  "background-image": "background2.png",
+  "show-comment-node-color": true,
+  "comment-font-color": [
+    255,
+    255,
+    255,
+    200
+  ],
+  "winrate-line-color": [
+    0,
+    255,
+    0,
+    200
+  ],
+  "blunder-node-colors": [
+    [
+      255,
+      0,
+      0,
+      200
+    ],
+    [
+      0,
+      255,
+      0,
+      200
+    ],
+    [
+      0,
+      0,
+      255
+    ],
+    [
+      255,
+      255,
+      0
+    ],
+    [
+      0,
+      255,
+      255
+    ],
+    [
+      255,
+      0,
+      255
+    ]
+  ]
+}
